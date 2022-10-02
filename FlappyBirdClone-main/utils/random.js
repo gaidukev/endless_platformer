9baa600aa5f4c3f890c7b1398aa23e722061e9dd
wilsontruong <90410232+wilsontruong@users.noreply.github.com>
2022-10-02 09:23:01 -0600

    pipe generation change

--- FlappyBirdClone-main/utils/random.js
+++ FlappyBirdClone-main/utils/random.js
@@ -3,11 +3,18 @@ import { Dimensions } from 'react-native'
 const windowHeight = Dimensions.get('window').height
 const windowWidth = Dimensions.get('window').width
 
+
 export const getRandom = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 export const getPipeSizePosPair = (addToPosX = 0) => {
-    let yPosTop = -getRandom(300, windowHeight - 100)
+    let jumpDuck = getRandom(0,3);
+    let yPosTop = 0;
+    if (jumpDuck <= 1) {
+        yPosTop = -60;
+    } else { 
+        yPosTop = -250;
+    }
 
     const pipeTop = { pos: { x: windowWidth + addToPosX, y: yPosTop }, size: { height: windowHeight * 2, width: 75 } }
     const pipeBottom = { pos: { x: windowWidth + addToPosX, y: windowHeight * 2 + 200 + yPosTop }, size: { height: windowHeight * 2, width: 75 } }
